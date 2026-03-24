0a1ebcf89bb7771eee7939347b54b4c2294bca35
heheeksdee0917 <sailor.moon_97@hotmail.com>
2026-03-24 11:02:34 +0800

    SEO updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ const samsungSharpSans = localFont({
 
 export const metadata: Metadata = {
   title: {
-    default: 'Best Interior Designer in KL - If you‘re looking for interior design service near me or Living Room, Kitchen or Commercial design near me - Voala Interior is the palce to be',
+    default: 'Best Interior Designer KL - If you‘re looking for interior design service near me or Living Room, Kitchen or Commercial design near me - Voala Interior is the palce to be',
     template: '%s - Best Interior Design in KL | Voala Interior',
   },
   description: "Malaysia's award-winning interior design & build studio in Kuala Lumpur – best one-stop service for luxury residential & commercial projects.",
